f23e14c16b9ac1d1b6f8e153484f6e10861a5cfd
Hussain Surti <surti.huss@gmail.com>
2022-11-16 16:02:37 +0400

    organize code, added lease method

--- lib/src/core/repository/app_respository.dart
+++ lib/src/core/repository/app_respository.dart
@@ -1,23 +1,26 @@
 import 'package:yourgaff_repo/src/core/models/landlord.dart';
+import 'package:yourgaff_repo/src/core/models/lease.dart';
 import 'package:yourgaff_repo/src/core/models/photo_group.dart';
 import 'package:yourgaff_repo/src/core/models/property.dart';
 import 'package:yourgaff_repo/src/core/models/rent_payment.dart';
 import 'package:yourgaff_repo/src/core/models/tenant.dart';
 
 abstract class AppRepository {
-  Future<List<PhotoGroup>> loadPhotoGroups();
-
-  Future<void> savePhotoGroup(PhotoGroup photoGroup);
-
   Future<Tenant> getCurrentTenant();
-
-  Future<void> updateCurrentTenant(Tenant tenant);
 
   Future<Landlord> getCurrentLandlord();
 
   Future<List<RentPayment>> getRentPayments();
 
-  Future<void> addRentPayment(RentPayment rentPayment);
-
   Future<Property> getCurrentProperty();
+
+  Future<Lease> getCurrentLease();
+
+  Future<List<PhotoGroup>> getPhotoGroups();
+
+  Future<void> savePhotoGroup(PhotoGroup photoGroup);
+
+  Future<void> updateCurrentTenant(Tenant tenant);
+
+  Future<void> addRentPayment(RentPayment rentPayment);
 }
