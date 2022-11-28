20f9f3c6d7b8af63460491107053b0be790e4cc7
Hussain Surti <surti.huss@gmail.com>
2022-11-28 13:53:24 +0400

    updated dashboard dto to have rent payment

--- lib/src/core/models/dashboard_dto.dart
+++ lib/src/core/models/dashboard_dto.dart
@@ -5,20 +5,31 @@ class DashboardDTO {
     required this.lease,
     required this.property,
     required this.tenant,
-    //required this.rentPayment,
+    required this.rentPayment,
   });
 
   factory DashboardDTO.fromJson(Map<String, dynamic> json) {
+    final List<RentPayment> rentPaymentList = [];
+    RentPayment? nextRentPayment;
+    for (final element in json['rentpayment'] as Iterable<dynamic>) {
+      rentPaymentList
+          .add(RentPayment.fromJson(element as Map<String, dynamic>));
+    }
+    if (rentPaymentList.isNotEmpty) {
+      nextRentPayment =
+          rentPaymentList.firstWhere((element) => element.status == 'unpaid');
+    }
+
     return DashboardDTO(
       lease: Lease.fromJson(json),
       property: Property.fromJson(json['property'] as Map<String, dynamic>),
       tenant: Tenant.fromJson(json['tenant'] as Map<String, dynamic>),
-      //rentPayment: RentPayment.fromJson(json['rentpayment'])
+      rentPayment: nextRentPayment,
     );
   }
 
   final Lease lease;
   final Property property;
   final Tenant tenant;
-  //final RentPayment rentPayment;
+  final RentPayment? rentPayment;
 }
